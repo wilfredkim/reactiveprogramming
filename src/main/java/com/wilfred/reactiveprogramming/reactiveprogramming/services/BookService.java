519d0ed44eb2d2f07b0b2c044f400c08bfaf5585
Wilfred <wilfredkim5@gmail.com>
2024-03-13 22:51:10 +0300

    updates

--- src/main/java/com/wilfred/reactiveprogramming/reactiveprogramming/services/BookService.java
+++ src/main/java/com/wilfred/reactiveprogramming/reactiveprogramming/services/BookService.java
@@ -4,9 +4,12 @@ import com.wilfred.reactiveprogramming.reactiveprogramming.domains.Book;
 import com.wilfred.reactiveprogramming.reactiveprogramming.domains.Review;
 import com.wilfred.reactiveprogramming.reactiveprogramming.exceptions.BookException;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -20,16 +23,49 @@ public class BookService {
         this.reviewService = reviewService;
     }
 
-    public Flux<Book> getBooks(){
+    public Flux<Book> getBooks() {
         var allBooks = bookInfoService.getBooks();
         return allBooks.flatMap(bookInfo -> {
-            Mono<List<Review>> reviews =reviewService.getReview(bookInfo.getBookId()).collectList();
-            return  reviews.map(reviews1 -> new Book(bookInfo,reviews1));
+            Mono<List<Review>> reviews = reviewService.getReview(bookInfo.getBookId()).collectList();
+            return reviews.map(reviews1 -> new Book(bookInfo, reviews1));
         }).onErrorMap(
                 throwable -> {
-                    log.info("Exception::::::::::::"+throwable);
+                    log.info("Exception::::::::::::" + throwable);
                     return new BookException("Exception occurred while fetching books!");
                 }
         ).log();
+    }
+
+    public Flux<Book> getBooksRetry() {
+        var allBooks = bookInfoService.getBooks();
+        return allBooks.flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews = reviewService.getReview(bookInfo.getBookId()).collectList();
+                    return reviews.map(reviews1 -> new Book(bookInfo, reviews1));
+                }).onErrorMap(
+                        throwable -> {
+                            log.info("Exception::::::::::::" + throwable);
+                            return new BookException("Exception occurred while fetching books!");
+                        }
+                ).retry(3)
+
+                .log();
+    }
+
+    public Flux<Book> getBooksRetryWhen() {
+        var retrySpecs = Retry.backoff(3, Duration.ofMillis(1000))
+                .onRetryExhaustedThrow(((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure())))
+                ;
+        var allBooks = bookInfoService.getBooks();
+        return allBooks.flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews = reviewService.getReview(bookInfo.getBookId()).collectList();
+                    return reviews.map(reviews1 -> new Book(bookInfo, reviews1));
+                }).onErrorMap(
+                        throwable -> {
+                            log.info("Exception::::::::::::" + throwable);
+                            return new BookException("Exception occurred while fetching books!");
+                        }
+                ).retryWhen(retrySpecs)
+
+                .log();
     }
 }
